6f36e6d750e9176c8356e2d147f2b923aaf319dd
Juliana de Carvalho <jucarvalho@outlook.com.br>
2023-02-21 13:48:21 -0300

    paginda home

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -6,7 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-
+  public appPages = [
+    { 
+      title: 'Home', 
+      url: '/pages/home', icon: 'mail' },
+    { 
+      title: 'Leis e Projetos publicos', 
+      url: '/pages/home', icon: 'mail' },
+    { 
+      title: 'Casas de apoio/ Serviços sociais', 
+      url: '/pages/home', icon: 'mail' },
+    { 
+      title: 'Por onde começar (Identificando uma violência domestica)', 
+      url: '/pages/home', icon: 'mail' },
+    { 
+      title: 'Contatos', 
+      url: '/pages/home', icon: 'bi bi-geo-alt' },
+  ];
   constructor() { }
 
   ngOnInit() {}
