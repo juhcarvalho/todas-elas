e94e14e917c547f965a7491f6438b92060bfbf99
Juliana de Carvalho <jucarvalho@outlook.com.br>
2023-02-21 11:23:57 -0300

    inicio

---
+++ src/app/pages/home/home.module.ts
@@ -0,0 +1,24 @@
+import { HeaderComponent } from '../../component/header/header.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { HomePageRoutingModule } from './home-routing.module';
+
+import { HomePage } from './home.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule
+  ],
+  declarations: [
+    HomePage,
+    HeaderComponent
+  ]
+})
+export class HomePageModule {}
